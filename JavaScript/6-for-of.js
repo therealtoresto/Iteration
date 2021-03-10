1cc023b760fde0a8601d0c51338c1288dab72ee6
TORESTO <therealtoresto@gmail.com>
2021-03-10 21:29:52 +0200

    replaced file

---
+++ JavaScript/6-for-of.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const numbers = [7, 10, 1, 5, 2];
+numbers.field2 = 'value2';
+numbers[-10] = 'value3';
+numbers.field1 = 'value1';
+numbers[5] = 20;
+
+for (const i of numbers) {
+    const value = numbers[i];
+    console.log(i, typeof i, value);
+}
