39d136f29fd65bd338c23186d91d5cad7f08b739
TORESTO <therealtoresto@gmail.com>
2021-03-10 21:51:34 +0200

    added file

---
+++ Exersise/2-for-of.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const sum = (...args) => {
+    let acc = 0;
+    for (const elem of args) acc += elem;
+    return acc;
+};
+
+// Usage
+
+const a = sum(1, 2, 3); // a === 6
+const b = sum(0); // b === 0
+const c = sum(); // c === 0
+const d = sum(1, -1, 1); // d === 1
+const e = sum(10, -1, -1, -1); // e === 7
+
+console.log(a, b, c, d, e);
