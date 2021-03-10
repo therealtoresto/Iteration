483613fe423482bb2e6a589b6639147470071e49
TORESTO <therealtoresto@gmail.com>
2021-03-10 22:05:46 +0200

    added file

---
+++ Exersise/4-do-while.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const sum = (...args) => {
+    let acc = 0;
+    if (!args.length) return 0;
+    do {
+        acc += args.pop();
+    } while (args.length > 0);
+    return acc;
+};
+
+// Usage
+
+const a = sum(1, 2, 3); // a === 6
+const b = sum(0); // b === 0
+const c = sum(); // c === 0
+const d = sum(1, -1, 1); // d === 1
+const e = sum(10, -1, -1, -1); // e === 7
+
+console.log(a, b, c, d, e);
